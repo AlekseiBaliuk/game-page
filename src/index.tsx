80629f158d24d5062e4e08c5d15592fdad9f3e2c
Oleksii Baliuk <oleksiibaliuk@Oleksiis-MacBook-Pro.local>
2026-02-09 14:56:49 +0200

    Add game

--- src/index.tsx
+++ src/index.tsx
@@ -5,17 +5,20 @@ import { GeoProvider } from "./app/providers/GeoProvider";
 import { AppThemeProvider } from "./app/providers/ThemeProvider";
 import App from "./app/App";
 import { GlobalStyles } from "./app/styles";
+import { StrictMode } from "react";
 
 const root = ReactDOM.createRoot(
   document.getElementById("root") as HTMLElement,
 );
 
 root.render(
-  <GeoProvider>
-    <AppThemeProvider>
-      <CssBaseline />
-      <GlobalStyles />
-      <App />
-    </AppThemeProvider>
-  </GeoProvider>,
+  <StrictMode>
+    <GeoProvider>
+      <AppThemeProvider>
+        <CssBaseline />
+        <GlobalStyles />
+        <App />
+      </AppThemeProvider>
+    </GeoProvider>
+  </StrictMode>,
 );
