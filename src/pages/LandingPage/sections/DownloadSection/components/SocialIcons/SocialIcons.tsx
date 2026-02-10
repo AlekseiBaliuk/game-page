029503d43b8b256ff6b8d14b88454a321e413d5c
Oleksii Baliuk <oleksiibaliuk@Oleksiis-MacBook-Pro.local>
2026-02-10 09:47:14 +0200

    Add game

--- src/pages/LandingPage/sections/DownloadSection/components/SocialIcons/SocialIcons.tsx
+++ src/pages/LandingPage/sections/DownloadSection/components/SocialIcons/SocialIcons.tsx
@@ -4,18 +4,49 @@ import TelegramIcon from "../../../../../../shared/assets/tg.png";
 import XIcon from "../../../../../../shared/assets/x.png";
 import Mail from "../../../../../../shared/assets/mail.png";
 import { useTranslate } from "../../../../../../shared/hooks/useTranslate";
+import { IconButton } from "@mui/material";
 
 export const SocialIcons = () => {
   const t = useTranslate();
+
+  const config = [
+    {
+      name: "Instagram",
+      url: "https://www.instagram.com",
+      icon: InstagramIcon,
+    },
+    {
+      name: "Telegram",
+      url: "https://t.me",
+      icon: TelegramIcon,
+    },
+    {
+      name: "X",
+      url: "https://www.x.com",
+      icon: XIcon,
+    },
+    {
+      name: "Mail",
+      url: "mailto:",
+      icon: Mail,
+    },
+  ];
 
   return (
     <Container>
       <SocialsText>{t("Us on social media")}:</SocialsText>
       <Wrapper>
-        <Icon src={InstagramIcon} alt="Instagram" />
-        <Icon src={TelegramIcon} alt="Telegram" />
-        <Icon src={XIcon} alt="X" />
-        <Icon src={Mail} alt="Mail" />
+        {config.map((item) => (
+          <IconButton
+            key={item.name}
+            href={item.url}
+            target="_blank"
+            rel="noopener noreferrer"
+            sx={{ padding: 0 }}
+          >
+            <Icon src={item.icon} alt={item.name} />
+          </IconButton>
+        ))}
       </Wrapper>
     </Container>
   );
